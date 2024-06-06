23cc53dda6f2c3c75e5dd3c8521c5c00cb90a4a0
Jence <56840349+ImranKhanPrince@users.noreply.github.com>
2024-06-06 12:30:05 +0600

    blink configured

---
+++ main/main.cpp
@@ -0,0 +1,46 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include "Arduino.h"
+#include "main.h"
+
+extern "C"
+{
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+}
+
+void app_main(void)
+{
+    void setup();
+
+    while (1)
+    {
+        loop();
+    }
+}
+
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+    // initialize digital pin LED_BUILTIN as an output.
+    pinMode(LED_BUILTIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+    delay(1000);                     // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+    delay(1000);                     // wait for a second
+}
---
+++ main/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN
+#define MAIN
+
+extern "C"
+{
+  void app_main(void);
+}
+
+void setup();
+void loop();
+
+#endif // MYCLASS_H
